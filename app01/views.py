cf22133127db5cb238dc4fc348c255ad904d909c
HoRizon <jiatenghuipl@gmail.com>
2020-07-03 23:03:25 +0800

    test

--- app01/views.py
+++ app01/views.py
@@ -1,26 +1,17 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django import forms
-
-from app01.models import Test
-
-
-class LoginForm(forms.Form):
-    user_name = forms.CharField(label="用户名", min_length=6, max_length=12)
-    user_password = forms.CharField(label="用户密码", min_length=8)
 
 
 # 登录
 def login(request):
     if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            if request.POST.get('user_name') == 'HoRizon' and request.POST.get('user_password') == '123456789':
-                request.session['username'] = request.POST.get('user_name')
-                request.session['is_login'] = True
-                return HttpResponseRedirect('/index/')
+        if request.POST.get('user_name') == 'HoRizon' and request.POST.get('user_password') == '123456789':
+            request.session['username'] = request.POST.get('user_name')
+            request.session['is_login'] = True
+            return HttpResponseRedirect('/index/')
+        else:
+            return HttpResponse("账号或密码错误,请返回")
     else:
-        form = LoginForm()
         return render(request, "login.html", locals())
 
 
